ca0f381c72dc9aafa1ab09d4215beeab0e627c74
Minkyu Park <rb6609@naver.com>
2024-07-23 14:03:33 +0900

    feat: 프로젝트생성

---
+++ src/test/java/com/example/arom/AromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
